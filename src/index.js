153f505da432058d2cfa7b5d1f9898d2124b8c5a
Zeke Zumbro <jezumbro@gmail.com>
2023-03-28 09:38:01 -0500

    update favicon

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import { createRoot } from 'react-dom/client'
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <head>
-      <link rel="icon" href="/public/favicon.ico" />
-    </head>
     <Layout>
       <App />
     </Layout>
