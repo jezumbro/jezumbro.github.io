001bbee71fef05979b334892ce4e4a427d509618
Zeke Zumbro <jezumbro@gmail.com>
2023-08-13 16:23:45 -0500

    adding posthog

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { App } from './App'
 import { Layout } from './components/layout/Layout'
 import './index.css'
+import posthog from 'posthog-js'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
+posthog.init('phc_1P0jj11b9N17pkpp9Z8CV5Wp7UHV8NZRlLunUFaU6pa', {
+  autocapture: false,
+})
 root.render(
   <React.StrictMode>
     <Layout>
