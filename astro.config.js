0c80030487ffac2e7e1419cb71a3896e82fa9ece
Zeke Zumbro <jezumbro@gmail.com>
2024-12-09 15:53:31 -0600

    feat: remove homestead and pdga link

--- astro.config.js
+++ astro.config.js
@@ -3,7 +3,9 @@ import react from '@astrojs/react'
 
 import tailwind from '@astrojs/tailwind'
 
+import mdx from '@astrojs/mdx'
+
 export default defineConfig({
   site: 'https://jezumbro.github.com',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
 })
