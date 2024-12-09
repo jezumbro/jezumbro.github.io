7c59c628582fd067734678787f3dd966007dda94
Zeke Zumbro <jezumbro@gmail.com>
2024-12-09 14:39:52 -0600

    feat: update site

--- astro.config.js
+++ astro.config.js
@@ -4,6 +4,6 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  site: 'jezumbro.github.com',
+  site: 'https://jezumbro.github.com',
   integrations: [react(), tailwind()],
 })
