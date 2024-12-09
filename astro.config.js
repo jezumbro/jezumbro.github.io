a232163501e0bf4e7d31ffbbe1ed59f438cb8808
Zeke Zumbro <jezumbro@gmail.com>
2024-12-09 14:36:18 -0600

    feat: add workflow

--- astro.config.js
+++ astro.config.js
@@ -4,5 +4,6 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
+  site: 'jezumbro.github.com',
   integrations: [react(), tailwind()],
 })
