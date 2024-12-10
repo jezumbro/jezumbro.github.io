12a747b3db351fe6bdb6ba0048161bbd2a6456a5
Zeke Zumbro <jezumbro@gmail.com>
2024-12-10 16:53:45 -0600

    feat: starting refactoring to shared layouts

---
+++ src/blog/post-1.md
@@ -0,0 +1,11 @@
+---
+title: 'My first MDX post'
+description: 'description'
+slug: 'first-post'
+pubDate: 2024-12-09
+author: 'Houston'
+---
+
+# Foo
+
+Written by: Zeke Zumbro
---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content'
+
+import { glob, file } from 'astro/loaders'
+
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/blog' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    slug: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    draft: z.boolean().optional(),
+  }),
+})
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { blog }
